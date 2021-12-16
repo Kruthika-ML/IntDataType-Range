ccc3ae72ad82d8839bef4428c89ec2e75b7a2373
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-16 11:27:15 +0530

    Add files via upload

---
+++ IntDataTypedemo.java
@@ -0,0 +1,16 @@
+//Primitive Data types-Integer
+public class IntDataTypedemo {
+    public static void main(String[] args){
+        byte maxByte = 127;
+        byte minByte = -128;
+
+        short maxShort = 32767;
+        short minShort = -32768;
+
+        int maxInt = 2147483647;
+        int minint = -2147483648;
+
+        long maxLong = 9223372036854775807L;
+        long minLong = -9223372036854775807L;
+    }
+}
